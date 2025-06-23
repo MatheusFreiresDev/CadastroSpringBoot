4b2f3a7f74ced7afa47954aba582f54bab0ed9d9
Matheus Freires <matheusfreires717@gmail.com>
2025-06-22 21:08:05 -0300

    build: Conecta o Banco de dados h2

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesRepository.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesRepository.java
@@ -1,4 +1,12 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
-public interface MissoesRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissoesRepository extends JpaRepository<MissoesModel, Long> {
+
+//Jpa - é uma abstrair(deixar mais facil) as Querrys
+
+
+
+
 }
