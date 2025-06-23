59c22864c35f4bf156f9fe1061ba6dc85faaca22
Matheus Freires <matheusfreires717@gmail.com>
2025-06-22 21:27:50 -0300

    feat: Adicionando lombok e arquivos

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -0,0 +1,25 @@
+package dev.java10x.CadastroDeNinjas.Missoes;
+
+import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "tb_missoes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class MissoesModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nome;
+    private String dificuldade;
+    @OneToMany(mappedBy = "missoes")
+    private List<NinjaModel> ninjas;
+
+}
---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -0,0 +1,31 @@
+package dev.java10x.CadastroDeNinjas.Ninjas;
+
+import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class NinjaModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nome;
+    @Column(unique = true)
+    private String email;
+    private int idade;
+
+   //Varios pra um. o varios seria a propia classe e o one a classe q vc red
+    @ManyToOne
+    @JoinColumn(name = "missoes_id") //chave estrangeira
+    private MissoesModel missoes;
+
+
+}
